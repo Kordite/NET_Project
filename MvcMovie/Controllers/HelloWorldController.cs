22a25d9da9c83dd2009d570d3c66dd7cd9ad59c1
Ryan Evoniuk <ryan.evoniuk@q2ebanking.com>
2018-07-16 08:01:13 -0500

    first commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "This is the default view...";
+        }
+
+        public string Welcome(string name, int numTimes)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
